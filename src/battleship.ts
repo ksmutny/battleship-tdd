ec1c89d1367051d7c393a39f17fed0a3a14ff386
Karel Smutny <karel.smutny@outlook.com>
2021-04-09 10:33:40 +0200

    Created empty Typescript project

---
+++ src/battleship.ts
@@ -0,0 +1,5 @@
+import * as readline from 'readline-sync'
+
+console.log('here we go')
+const answer = readline.question('Hello? ')
+console.log(answer)
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { assert } from 'chai'
+
+
+describe('sample test', () => {
+    it('should pass', () => {
+        assert.isTrue(true)
+    })
+})
